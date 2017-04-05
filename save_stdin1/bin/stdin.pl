44bf7e4dba533efd97be46f42312063c257073b6
VKoorits <koorits.viktor@yandex.ru>
2017-04-05 12:19:02 +0300

    stdin_save

---
+++ save_stdin1/bin/stdin.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use Getopt::Long;
+use Data::Dumper;
+use DDP;
+
+my $param = {};
+GetOptions($param, 'file=s');
+open( my $fh, '>', $param->{file} ) or die 'no this file';
+
+
+
+sub signal1 {
+	print STDERR 'Double Ctrl+C for exit';
+	$SIG{INT} = sub{
+		close($fh);
+		exit;
+	};
+};
+
+$SIG{INT} = \&signal1;
+
+print "Get ready\n";
+my $count_str = 0;
+my $data_size = 0;
+
+while(<>){ print $fh $_ ; $data_size += length($_)-1; $count_str++; }
+print $data_size." ".$count_str." ".int($data_size / $count_str);
+
