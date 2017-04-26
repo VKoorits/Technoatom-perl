1a196c0b780cc93618b61f5af208b03ef8f9b8da
VKoorits <koorits.viktor@yandex.ru>
2017-04-26 10:29:35 +0300

    corrected proxy

--- proxy/proxy_server.pl
+++ proxy/proxy_server.pl
@@ -5,19 +5,38 @@ use AnyEvent::Handle;
 use AnyEvent::HTTP;
 use 5.010;
 use Data::Dumper;
+use DDP;
 
 sub worker{
 	my ($handler, $request, $url) = @_;
+	$url //= 'https://www.google.ru/';
 	say $request;
 	
 	if($request =~ m/^URL\s(?<url>[\w\/\.\:]+)/){
 		$url = $1;
-	$handler->push_write ("OK\n");
-	say 'OK';
+		$handler->push_write ("OK\n");
+		say 'OK';
 	}elsif($request eq 'HEAD') {
-		http_head($url, sub{ $handler->push_write( Dumper $_[1]); say 'OK';} );
+		http_head($url, sub{
+			my $answer = "";
+			while(1) {
+				my ($k, $v) = each %{$_[1]};
+				if( defined $k) {
+					unless (ref $v) {
+						$answer .= $k.': '.$v."\n";
+					}
+				} else {
+					$handler->push_write( 'OK '.length($answer)."\n".$answer );
+					last;
+				}
+			}
+			say 'OK';
+		});
 	}elsif($request eq 'GET') {
-		http_get($url, sub{ $handler->push_write( $_[0]."\n"); say 'OK'; } );
+		http_get($url, sub{
+			$handler->push_write( "OK ".$_[1]->{'content-length'}."\n".$_[0]."\n");
+			say 'OK '.$_[1]->{'content-length'}; 
+		});
 	}elsif($request eq 'FIN') {
 		close $handler->fh;		
 		say 'OK closed';
@@ -44,7 +63,7 @@ tcp_server '127.0.0.1', 8081, sub {
 			$handler->destroy; # destroy handle
 			AE::log info => "Done.";
 		};
-	$handler->push_read (line => \&worker);	
+	$handler->push_read (line => sub{ worker($_[0], $_[1], undef) });	
 };
 
 AE::cv->recv();
